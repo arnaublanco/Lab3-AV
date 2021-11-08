b07145ebf03670182b7734ed6f624d5cbb5568ab
Arnau <blancoarnau@gmail.com>
2021-11-08 18:32:32 +0100

    shader file

--- data/shaders/normal.fs
+++ data/shaders/normal.fs
@@ -5,7 +5,11 @@ varying vec3 v_normal;
 varying vec2 v_uv;
 varying vec4 v_color;
 
+uniform sampler3D volume;
+
 void main()
 {
-	gl_FragColor = vec4(v_normal,1.0);
+	//vec3 direction = v_world_position - u_camera_position;
+	float d = texture3D(volume,vec3(1.0)).x;
+	gl_FragColor = vec4(texture3D(volume,vec3(1.0).xyz,1.0);
 }
