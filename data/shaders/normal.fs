d1530931b3226038dd98589b3ee9f47760992a37
PaulaO22 <77243736+PaulaO22@users.noreply.github.com>
2021-11-14 12:36:16 +0100

    update normal.fs

--- data/shaders/normal.fs
+++ data/shaders/normal.fs
@@ -14,7 +14,7 @@ uniform mat4 model;
 uniform mat4 inv_model;
 
 vec3 local_to_texture(vec3 v_local){
-	vec3 v_texture = (v_local + 1)/2;
+	vec3 v_texture = (v_local + 1.0)/2.0;
 	return v_texture;
 }
 
@@ -29,27 +29,25 @@ void main()
 {
 	vec3 sample_pos = v_position; // Sample position
 	vec3 ray_dir = normalize(v_world_position - u_camera_position); // Ray direction
-	ray_dir = local_to_texture(world_to_local(ray_dir));
-	//vec3 sample_pos_local = world_to_local(sample_pos);
+	ray_dir = world_to_local(ray_dir);
 
 	float d = texture3D(volume, local_to_texture(sample_pos)).x; // Density of sample position
 	vec4 sample_color = vec4(d,d,d,d);
-	vec4 final_color = vec4(0.0) + epsilon;
+	vec4 final_color = vec4(0.0);
 
 	int count = 0;
 	while(sample_pos.x >= -1 && sample_pos.y >= -1 && sample_pos.z >= -1 && sample_pos.x <= 1 && sample_pos.y <= 1 && sample_pos.z <= 1 && final_color.a <= 1 && count <= N_MAX){
 		
-		sample_color = vec4(d,1-d,0,d*d);
-		sample_color.rgb *= sample_color.a;
 		final_color += ray_step * (1.0 - final_color.a) * sample_color;
 
 		d = texture3D(volume, local_to_texture(sample_pos)).x;
+		sample_color = vec4(d,d,d,d);
+		sample_color.rgb *= sample_color.a;
 		sample_pos += ray_dir * ray_step;
-		//sample_pos_local = world_to_local(sample_pos);
 		
 		count = count + 1;
 	}
-	//vec3 T = texture3D(volume,vec3(1.0));
+	//vec4 T = texture3D(volume,v_position.xyz);
 	//gl_FragColor = vec4(T,1.0);
 	gl_FragColor = final_color;
 }
