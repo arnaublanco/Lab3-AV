c40967873a1828363122ec5be089367304084db8
Arnau <blancoarnau@gmail.com>
2021-11-22 19:49:02 +0100

    for loop implemented instead of while loop
    nse perq es veu raro a lordinador de la uni, espero que es vegi be quan ho corri al pc de casa

--- data/shaders/normal.fs
+++ data/shaders/normal.fs
@@ -1,4 +1,4 @@
-#define N_MAX 1000
+#define N_MAX 1000000
 
 varying vec3 v_position;
 varying vec3 v_world_position;
@@ -64,60 +64,50 @@ void main()
 	float texture_width = 1.0; // Texture width
 	float offset = texture2D(u_jitter_texture, gl_FragCoord.xy / texture_width).x; // Offset for the jittering
 
-	if(u_vis_type == 0.0){
+	sample_pos += ray_dir * offset; // Sample position with jittering offset
 
-		sample_pos += ray_dir * offset; // Sample position with jittering offset
+	float d = texture3D(volume, local_to_texture(sample_pos)).x; // Density of sample position
+	vec2 v = vec2(clamp(d,0.01,0.99),0.0); // 2-D vector for retrieving color based on the density
 
-		float d = texture3D(volume, local_to_texture(sample_pos)).x; // Density of sample position
-		vec2 v = vec2(d,0.0); // 2-D vector for retrieving color based on the density
-		vec4 sample_color = vec4(d,d,d,d);
-		//vec4 sample_color = texture2D(u_tfLUT,v); // Sample color based on the density
+	vec4 sample_color = vec4(0.0);
 
-		int count = 0; // Counter to count the number of iterations
-
-		// Check if the sample position is inside the boundaries of the box
-		while(all(greaterThan(sample_pos.xyz, vec3(-1.0))) && all(lessThan(sample_pos.xyz, vec3(1.0))) && final_color.a <= 1.0 && count <= N_MAX){
+	// Check if the sample position is inside the boundaries of the box
+	for(int count = 0; count <= N_MAX; count++){
 		
-			// If the samples are located below (<0) the plane
-			if(a*sample_pos.x + b*sample_pos.y + c*sample_pos.z + d_plane <= 0){
-				final_color += ray_step * (1.0 - final_color.a) * sample_color; // Compute final color (accumulation)
-				d = texture3D(volume, local_to_texture(sample_pos)).x; // Calculate density based on the sample position
-				sample_color = vec4(d,d,d,d);
-				v = vec2(d,0.0); // 2-D for color retrieval
-				//sample_color = texture2D(u_tfLUT,v); // Sample color based on the density
-				sample_color.rgb *= sample_color.a; // Homogeneous coordinates
-			}
-			sample_pos += ray_dir * ray_step; // Update sample position
-	
-			count = count + 1; // Add up counter
-		}
+		// If the samples are located below (<0) the plane
+		if(a*sample_pos.x + b*sample_pos.y + c*sample_pos.z + d_plane <= 0.0){
 
-		// If the alpha channel of the final is less than epsilon, then discard final color
-		if(final_color.a < epsilon){
-			discard;
+			final_color += ray_step * (1.0 - final_color.a) * sample_color; // Compute final color (accumulation)
+
+			d = texture3D(volume, local_to_texture(sample_pos)).x; // Calculate density based on the sample position
+
+			if(u_vis_type == 1.0){
+				sample_color = texture2D(u_tfLUT,v);
+			}else{
+				sample_color = vec4(d,d,d,d);
+			}
+			
+			v = vec2(clamp(d,0.01,0.99),0.0); // 2-D for color retrieval
+			sample_color.rgb *= sample_color.a; // Homogeneous coordinates
+		}
+		sample_pos += ray_dir * ray_step; // Update sample position
+	
+		if(any(lessThanEqual(sample_pos.xyz, vec3(-1.0))) || any(greaterThanEqual(sample_pos.xyz, vec3(1.0))) || final_color.a >= 1.0){
+			break;
 		}
 
 		final_color *= brightness; // Add a brightness factor to final color
-
-	}else{
-
-		int count = 0; // Counter to count the number of iterations
-
-		sample_pos += ray_dir * offset;
-
-		while(all(greaterThan(sample_pos.xyz, vec3(-1.0))) && all(lessThan(sample_pos.xyz, vec3(1.0))) && final_color.a <= 1.0){
-			if(a*sample_pos.x + b*sample_pos.y + c*sample_pos.z + d_plane <= 0){
-				final_color = vec4(computeGradient(sample_pos),1.0);
-			}
-			sample_pos += ray_dir * ray_step; // Update sample position
-			count = count + 1; // Add up counter
-		}
-
-		if(final_color.a < epsilon){
-			discard;
-		}
 		
 	}
+
+		//while(all(greaterThan(sample_pos.xyz, vec3(-1.0))) && all(lessThan(sample_pos.xyz, vec3(1.0))) && final_color.a <= 1.0){
+		//	if(a*sample_pos.x + b*sample_pos.y + c*sample_pos.z + d_plane <= 0 ){
+		//		final_color = vec4(computeGradient(sample_pos),1.0);
+		//	}
+		//	sample_pos += ray_dir * ray_step; // Update sample position
+		//	count = count + 1; // Add up counter
+		//}
+
 	gl_FragColor = final_color;
 
 }
