88c606d959d448973808e6b45e225070ec3972c7
arnau.blanco01@estudiant.upf.edu <blancoarnau@gmail.com>
2021-11-28 11:12:46 +0100

    histograms

--- data/volumes/histograms.m
+++ data/volumes/histograms.m
@@ -1,6 +1,30 @@
+
 I1 = imread("foot_16_16.png");
-H1 = histcounts(I1);
-[pks, idx] = findpeaks(H1);
+I2 = imread("teapot_16_16.png");
+I3 = imread("brain_16_16.png");
+I4 = imread("bonsai_16_16.png");
+
+%%
+
+figure;
+
+H1 = histcounts(I1,'NumBins',256);
 plot(H1);
-hold on;
-scatter(idx,pks);
+
+%%
+figure;
+
+H2 = histcounts(I2,'NumBins',256);
+plot(H2);
+
+%%
+figure;
+
+H3 = histcounts(I3,'NumBins',256);
+plot(H3);
+
+%%
+figure;
+
+H4 = histcounts(I4,'NumBins',256);
+plot(H4);
